11dc424b7d865c198794e00c2133b6035b03f53d
Kyle Krull <kkrull@8thlight.com>
2018-04-09 16:23:31 -0500

    Test fixture for the file system

--- http/get_request_test.go
+++ http/get_request_test.go
@@ -1,18 +1,37 @@
 package http_test
 
 import (
-	"github.com/kkrull/gohttp/http"
+	"os"
+	"path"
 
+	"github.com/kkrull/gohttp/http"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("GetRequest", func() {
-	It("accepts a base path", func() {
-		request := http.GetRequest{
-			BaseDirectory: "/tmp",
-			Target:  "/",
-			Version: "HTTP/1.1"}
-		Expect(request).NotTo(BeNil())
+	Describe("#Handle", func() {
+		var (
+			basePath string
+		)
+
+		BeforeEach(func() {
+			basePath = makeEmptyTestDirectory("GetRequest", os.ModePerm)
+		})
+
+		It("accepts a base path", func() {
+			request := http.GetRequest{
+				BaseDirectory: "/tmp",
+				Target:        "/",
+				Version:       "HTTP/1.1"}
+			Expect(request).NotTo(BeNil())
+		})
 	})
 })
+
+func makeEmptyTestDirectory(testName string, fileMode os.FileMode) string {
+	testPath := path.Join(".test", testName)
+	Expect(os.RemoveAll(testPath)).To(Succeed())
+	Expect(os.MkdirAll(testPath, fileMode)).To(Succeed())
+	return testPath
+}
