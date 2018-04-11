d701e4fcc33635530454ebc65d1f9c269b0a885d
Kyle Krull <kkrull@8thlight.com>
2018-04-11 15:55:42 -0500

    Standarized to the Response interface

--- http/get_request.go
+++ http/get_request.go
@@ -20,38 +20,38 @@ func (request *GetRequest) Handle(client io.Writer) error {
 	resolvedTarget := path.Join(request.BaseDirectory, request.Target)
 	info, err := os.Stat(resolvedTarget)
 	if err != nil {
-		response := &NotFoundResponse{client: client}
-		response.IssueForTarget(request.Target)
+		response := &NotFoundResponse{Target: request.Target}
+		response.WriteTo(client)
 	} else if info.IsDir() {
 		files, _ := ioutil.ReadDir(resolvedTarget)
-		response := &DirectoryListingResponse{client: client}
-		response.IssueForFiles(files)
+		response := &DirectoryListingResponse{Files: files}
+		response.WriteTo(client)
 	} else {
-		response := &TextFileContentsResponse{client: client}
-		response.IssueForFile(resolvedTarget)
+		response := &TextFileContentsResponse{Filename: resolvedTarget}
+		response.WriteTo(client)
 	}
 
 	return nil
 }
 
 type DirectoryListingResponse struct {
-	client io.Writer
+	Files []os.FileInfo
 }
 
-func (response DirectoryListingResponse) IssueForFiles(files []os.FileInfo) {
-	writeStatusLine(response.client, 200, "OK")
-	writeHeader(response.client, "Content-Type", "text/plain")
+func (response DirectoryListingResponse) WriteTo(client io.Writer) {
+	writeStatusLine(client, 200, "OK")
+	writeHeader(client, "Content-Type", "text/plain")
 
-	message := messageListingFiles(files)
-	writeHeader(response.client, "Content-Length", strconv.Itoa(message.Len()))
-	writeEndOfMessageHeader(response.client)
+	message := response.messageListingFiles()
+	writeHeader(client, "Content-Length", strconv.Itoa(message.Len()))
+	writeEndOfMessageHeader(client)
 
-	writeBody(response.client, message.String())
+	writeBody(client, message.String())
 }
 
-func messageListingFiles(files []os.FileInfo) *bytes.Buffer {
+func (response DirectoryListingResponse) messageListingFiles() *bytes.Buffer {
 	message := &bytes.Buffer{}
-	for _, file := range files {
+	for _, file := range response.Files {
 		fmt.Fprintf(message, "%s\n", file.Name())
 	}
 
@@ -59,22 +59,22 @@ func messageListingFiles(files []os.FileInfo) *bytes.Buffer {
 }
 
 type TextFileContentsResponse struct {
-	client io.Writer
+	Filename string
 }
 
-func (response TextFileContentsResponse) IssueForFile(filename string) {
-	writeStatusLine(response.client, 200, "OK")
-	writeHeadersDescribingFile(response, filename)
-	writeEndOfMessageHeader(response.client)
+func (response TextFileContentsResponse) WriteTo(client io.Writer) {
+	writeStatusLine(client, 200, "OK")
+	response.writeHeadersDescribingFile(client)
+	writeEndOfMessageHeader(client)
 
-	file, _ := os.Open(filename)
-	copyToBody(response.client, file)
+	file, _ := os.Open(response.Filename)
+	copyToBody(client, file)
 }
 
-func writeHeadersDescribingFile(response TextFileContentsResponse, filename string) {
-	writeHeader(response.client, "Content-Type", contentTypeFromFileExtension(filename))
-	info, _ := os.Stat(filename)
-	writeHeader(response.client, "Content-Length", strconv.FormatInt(info.Size(), 10))
+func (response TextFileContentsResponse) writeHeadersDescribingFile(client io.Writer) {
+	writeHeader(client, "Content-Type", contentTypeFromFileExtension(response.Filename))
+	info, _ := os.Stat(response.Filename)
+	writeHeader(client, "Content-Length", strconv.FormatInt(info.Size(), 10))
 }
 
 func contentTypeFromFileExtension(filename string) string {
@@ -87,18 +87,18 @@ func contentTypeFromFileExtension(filename string) string {
 }
 
 type NotFoundResponse struct {
-	client io.Writer
+	Target string
 }
 
-func (response NotFoundResponse) IssueForTarget(requestTarget string) {
-	writeStatusLine(response.client, 404, "Not Found")
-	writeHeader(response.client, "Content-Type", "text/plain")
+func (response NotFoundResponse) WriteTo(client io.Writer) {
+	writeStatusLine(client, 404, "Not Found")
+	writeHeader(client, "Content-Type", "text/plain")
 
-	message := fmt.Sprintf("Not found: %s", requestTarget)
-	writeHeader(response.client, "Content-Length", strconv.Itoa(len(message)))
-	writeEndOfMessageHeader(response.client)
+	message := fmt.Sprintf("Not found: %s", response.Target)
+	writeHeader(client, "Content-Length", strconv.Itoa(len(message)))
+	writeEndOfMessageHeader(client)
 
-	writeBody(response.client, message)
+	writeBody(client, message)
 }
 
 func writeStatusLine(client io.Writer, status int, reason string) {
