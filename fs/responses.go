1f680bd63969e94e93d45d712c1a32e31148da98
Kyle Krull <kkrull@8thlight.com>
2018-04-12 11:54:14 -0500

    Extracted methods to write HTML page

--- fs/responses.go
+++ fs/responses.go
@@ -30,17 +30,41 @@ func (listing DirectoryListing) WriteTo(client io.Writer) error {
 
 func (listing DirectoryListing) messageListingFiles() *bytes.Buffer {
 	message := &bytes.Buffer{}
-	message.WriteString("<html>")
-	message.WriteString("<head><title>gohttp</title></head>")
-
-	message.WriteString("<body>")
-	for _, file := range listing.Files {
-		fmt.Fprintf(message, "<a href=\"/%s\">%s</a>", file, file)
-	}
-
-	message.WriteString("</body>")
+	message.WriteString("<html>\n")
+	listing.writeHead(message)
+	listing.writeBody(message)
 	message.WriteString("</html>")
 	return message
+}
+
+func (listing DirectoryListing) writeHead(message *bytes.Buffer) {
+	message.WriteString("<head>\n")
+	message.WriteString("<title>gohttp</title>\n")
+	message.WriteString("</head>\n")
+}
+
+func (listing DirectoryListing) writeBody(message *bytes.Buffer) {
+	message.WriteString("<body>\n")
+	listing.writeFileListing(message)
+	message.WriteString("</body>\n")
+}
+
+func (listing DirectoryListing) writeFileListing(message *bytes.Buffer) {
+	message.WriteString("<ul>\n")
+	for _, file := range listing.Files {
+		message.WriteString("  " + makeListItem(makeLink(file)))
+		message.WriteString("\n")
+	}
+
+	message.WriteString("</ul>\n")
+}
+
+func makeListItem(text string) string {
+	return fmt.Sprintf("<li>%s</li>", text)
+}
+
+func makeLink(filename string) string {
+	return fmt.Sprintf("<a href=\"/%s\">%s</a>", filename, filename)
 }
 
 type FileContents struct {
