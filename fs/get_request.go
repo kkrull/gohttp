e959cea50d4181e243f731fd0802daaff7ae01fd
Kyle Krull <kkrull@8thlight.com>
2018-04-11 20:53:48 -0500

    Extracted methods

--- fs/get_request.go
+++ fs/get_request.go
@@ -16,23 +16,32 @@ type GetRequest struct {
 
 func (request *GetRequest) Handle(client io.Writer) error {
 	resolvedTarget := path.Join(request.BaseDirectory, request.Target)
+	response := request.determineResponse(resolvedTarget)
+	response.WriteTo(client)
+	return nil
+}
+
+func (request *GetRequest) determineResponse(resolvedTarget string) getresponse {
 	info, err := os.Stat(resolvedTarget)
 	if err != nil {
-		notFound := &clienterror.NotFound{Target: request.Target}
-		notFound.WriteTo(client)
+		return &clienterror.NotFound{Target: request.Target}
 	} else if info.IsDir() {
 		files, _ := ioutil.ReadDir(resolvedTarget)
-		fileNames := make([]string, len(files))
-		for i, file := range files {
-			fileNames[i] = file.Name()
-		}
-
-		directoryListing := &DirectoryListing{Files: fileNames}
-		directoryListing.WriteTo(client)
+		return &DirectoryListing{Files: readFileNames(files)}
 	} else {
-		fileContents := &FileContents{Filename: resolvedTarget}
-		fileContents.WriteTo(client)
+		return &FileContents{Filename: resolvedTarget}
+	}
+}
+
+func readFileNames(files []os.FileInfo) []string {
+	fileNames := make([]string, len(files))
+	for i, file := range files {
+		fileNames[i] = file.Name()
 	}
 
-	return nil
+	return fileNames
+}
+
+type getresponse interface {
+	WriteTo(client io.Writer) error
 }
