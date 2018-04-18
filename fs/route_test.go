aa2a2d76b738477318473158393558ec0388b572
Kyle Krull <kkrull@8thlight.com>
2018-04-18 11:18:00 -0500

    Added missing tests on route

--- fs/route_test.go
+++ fs/route_test.go
@@ -9,18 +9,35 @@ import (
 
 var _ = Describe("route", func() {
 	var (
-		router = fs.NewRoute("/public")
+		router     = fs.NewRoute("/public")
+		controller = &fs.Controller{BaseDirectory: "/public"}
 	)
 
 	Describe("#Route", func() {
 		It("routes GET requests to GetRequest", func() {
 			requested := &http.RequestLine{Method: "GET", Target: "/foo"}
-			Expect(router.Route(requested)).To(BeEquivalentTo(
+			routedRequest := router.Route(requested)
+			Expect(routedRequest).To(BeEquivalentTo(
 				&fs.GetRequest{
-					Controller: &fs.Controller{BaseDirectory: "/public"},
+					Controller: controller,
 					Target:     "/foo",
 				}))
 		})
-		XIt("routes HEAD requests to HeadRequest")
+
+		It("routes HEAD requests to HeadRequest", func() {
+			requested := &http.RequestLine{Method: "HEAD", Target: "/foo"}
+			routedRequest := router.Route(requested)
+			Expect(routedRequest).To(BeEquivalentTo(
+				&fs.HeadRequest{
+					Controller: controller,
+					Target:     "/foo",
+				}))
+		})
+
+		It("passes on any other method by returning nil", func() {
+			requested := &http.RequestLine{Method: "TRACE", Target: "/foo"}
+			routedRequest := router.Route(requested)
+			Expect(routedRequest).To(BeNil())
+		})
 	})
 })
