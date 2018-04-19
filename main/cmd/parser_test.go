a1b2115153bc6457f211ac0324ecef03856c98ec
Kyle Krull <kkrull@8thlight.com>
2018-04-19 14:59:58 -0500

    Removing duplication in tests

--- main/cmd/parser_test.go
+++ main/cmd/parser_test.go
@@ -47,36 +47,34 @@ var _ = Describe("CliCommandParser", func() {
 			})
 		})
 
-		Context("given unrecognized arguments", func() {
-			It("returns an ErrorCommand", func() {
-				errorCommand := &CliCommandMock{}
+		Describe("parsing failures", func() {
+			var errorCommand *CliCommandMock
+
+			BeforeEach(func() {
+				errorCommand = &CliCommandMock{}
 				factory = &CommandFactoryMock{ErrorCommandReturns: errorCommand}
 				parser = &cmd.CliCommandParser{Factory: factory}
-
-				returned = parser.Parse([]string{"gohttp", "--bogus"})
-				Expect(returned).To(BeIdenticalTo(errorCommand))
 			})
-		})
 
-		Context("when the path is missing", func() {
-			It("returns an ErrorCommand stating that the path is missing", func() {
-				errorCommand := &CliCommandMock{}
-				factory = &CommandFactoryMock{ErrorCommandReturns: errorCommand}
-				parser = &cmd.CliCommandParser{Factory: factory}
-
-				returned = parser.Parse([]string{"gohttp", "-p", "4242"})
-				factory.ErrorCommandShouldHaveReceived(fmt.Errorf("missing path"))
+			Context("given unrecognized arguments", func() {
+				It("returns an ErrorCommand", func() {
+					returned = parser.Parse([]string{"gohttp", "--bogus"})
+					Expect(returned).To(BeIdenticalTo(errorCommand))
+				})
 			})
-		})
 
-		Context("when the port is missing", func() {
-			It("returns ErrorCommand", func() {
-				errorCommand := &CliCommandMock{}
-				factory = &CommandFactoryMock{ErrorCommandReturns: errorCommand}
-				parser = &cmd.CliCommandParser{Factory: factory}
+			Context("when the path is missing", func() {
+				It("returns an ErrorCommand stating that the path is missing", func() {
+					returned = parser.Parse([]string{"gohttp", "-p", "4242"})
+					factory.ErrorCommandShouldHaveReceived(fmt.Errorf("missing path"))
+				})
+			})
 
-				returned = parser.Parse([]string{"gohttp", "-d", "/tmp"})
-				factory.ErrorCommandShouldHaveReceived(fmt.Errorf("missing port"))
+			Context("when the port is missing", func() {
+				It("returns an ErrorCommand stating that the port is missing", func() {
+					returned = parser.Parse([]string{"gohttp", "-d", "/tmp"})
+					factory.ErrorCommandShouldHaveReceived(fmt.Errorf("missing port"))
+				})
 			})
 		})
 	})
