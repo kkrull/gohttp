72850c87f3173893afccf98b5f01cb1344914f6a
Kyle Krull <kkrull@8thlight.com>
2018-04-21 13:10:15 -0500

    Documenting all the packages to make their purpose more clear

---
+++ main/cmd/doc.go
@@ -0,0 +1,5 @@
+// The "main" package, that does command-line parsing and application initialization
+//
+// This package is here as a convenience to allow the main package to be split up into multiple files,
+// while retaining the ability to do a simple build with `go build main.go`.
+package cmd
