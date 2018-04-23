2d0d28378b318674f172c6878685704660f28681
Kyle Krull <kkrull@8thlight.com>
2018-04-23 13:57:19 -0500

    The 5 methods expected at /method_options

--- playground/stateless_options.go
+++ playground/stateless_options.go
@@ -1,11 +1,17 @@
 package playground
 
-import "io"
+import (
+	"io"
+
+	"github.com/kkrull/gohttp/msg"
+)
 
 type StatelessOptionController struct {
 }
 
 func (controller *StatelessOptionController) Options(client io.Writer, target string) {
-	//TODO KDK: Copy-paste the response from capability/, then see if it can be extracted to an http.Response implementation
-	panic("implement me")
+	msg.WriteStatusLine(client, 200, "OK")
+	msg.WriteContentLengthHeader(client, 0)
+	msg.WriteHeader(client, "Allow", "GET,HEAD,POST,OPTIONS,PUT")
+	msg.WriteEndOfMessageHeader(client)
 }
--- playground/stateless_options_test.go
+++ playground/stateless_options_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/kkrull/gohttp/httptest"
 	"github.com/kkrull/gohttp/playground"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("StatelessOptions", func() {
@@ -17,13 +18,32 @@ var _ = Describe("StatelessOptions", func() {
 
 	Describe("#Options", func() {
 		BeforeEach(func() {
-			responseBuffer = &bytes.Buffer{}
-			controller = &playground.StatelessOptionController{}
 		})
 
-		It("responds with the 5 methods in cob_spec", func() {
-			controller.Options(responseBuffer, "/anywhere")
-			response = httptest.ParseResponse(responseBuffer)
+		Context("given a request for /method_options", func() {
+			BeforeEach(func() {
+				responseBuffer = &bytes.Buffer{}
+				controller = &playground.StatelessOptionController{}
+
+				controller.Options(responseBuffer, "/method_options")
+				response = httptest.ParseResponse(responseBuffer)
+			})
+
+			It("writes a well-formed response", func() {
+				response.ShouldBeWellFormed()
+			})
+			It("sets status to 200 OK", func() {
+				response.StatusShouldBe(200, "OK")
+			})
+			It("sets Content-Length to 0", func() {
+				response.HeaderShould("Content-Length", Equal("0"))
+			})
+			It("sets Allow to the methods that SimpleOption expects for this route", func() {
+				response.HeaderShould("Allow", Equal("GET,HEAD,POST,OPTIONS,PUT"))
+			})
+			It("has no body", func() {
+				response.BodyShould(BeEmpty())
+			})
 		})
 	})
 })
