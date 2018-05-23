5919b32ae28f5a5d1e4d5f8789bd435eab493645
Kyle Krull <kkrull@8thlight.com>
2018-05-23 08:32:34 -0500

    Stubbing in log.Viewer

--- log/log_route.go
+++ log/log_route.go
@@ -1,13 +1,32 @@
 package log
 
-import "github.com/kkrull/gohttp/http"
+import (
+	"github.com/kkrull/gohttp/http"
+)
 
 func NewLogRoute(path string) http.Route {
-	return &Route{}
+	return &Route{
+		Path:   path,
+		Viewer: &Viewer{},
+	}
 }
 
-type Route struct{}
+type Route struct {
+	Path   string
+	Viewer *Viewer
+}
 
 func (route *Route) Route(requested http.RequestMessage) http.Request {
-	panic("implement me")
+	if requested.Path() != route.Path {
+		return nil
+	}
+
+	return requested.MakeResourceRequest(route.Viewer)
+}
+
+// Views server logs
+type Viewer struct{}
+
+func (viewer *Viewer) Name() string {
+	return "Log viewer"
 }
--- log/log_route_test.go
+++ log/log_route_test.go
@@ -1,8 +1,12 @@
 package log_test
 
 import (
+	"bytes"
+
 	"github.com/kkrull/gohttp/http"
+	"github.com/kkrull/gohttp/httptest"
 	"github.com/kkrull/gohttp/log"
+	"github.com/kkrull/gohttp/msg/clienterror"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -11,18 +15,48 @@ var _ = Describe("::NewLogRoute", func() {
 	It("returns a Route at the given path", func() {
 		route := log.NewLogRoute("/foo")
 		Expect(route).NotTo(BeNil())
-		Expect(route).To(BeEquivalentTo(&log.Route{}))
+		Expect(route).To(BeEquivalentTo(&log.Route{
+			Path:   "/foo",
+			Viewer: &log.Viewer{},
+		}))
 	})
 })
 
 var _ = Describe("Route", func() {
+	const (
+		configuredPath = "/logs"
+	)
+
 	var (
-		router http.Route
+		router   http.Route
+		response = &bytes.Buffer{}
 	)
 
 	Describe("#Route", func() {
 		BeforeEach(func() {
-			router = &log.Route{}
+			router = &log.Route{Path: configuredPath}
+			response.Reset()
+		})
+
+		Context("when the path is the given path", func() {
+			Context("when the method is OPTIONS", func() {
+				BeforeEach(func() {
+					requested := http.NewOptionsMessage(configuredPath)
+					routedRequest := router.Route(requested)
+					Expect(routedRequest).NotTo(BeNil())
+					routedRequest.Handle(response)
+				})
+
+				It("responds 200 OK with no body", httptest.ShouldHaveNoBody(response, 200, "OK"))
+				It("allows OPTIONS", httptest.ShouldAllowMethods(response, http.OPTIONS))
+				It("allows GET", httptest.ShouldAllowMethods(response, http.GET))
+			})
+
+			It("replies Method Not Allowed on any other method", func() {
+				requested := http.NewTraceMessage(configuredPath)
+				routedRequest := router.Route(requested)
+				Expect(routedRequest).To(BeAssignableToTypeOf(clienterror.MethodNotAllowed()))
+			})
 		})
 
 		It("passes on any other path by returning nil", func() {
